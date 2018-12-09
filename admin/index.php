a1067e7ae60e47bd5fd0454a3da29a7233de2cff
walacetael <walacesilvatael@gmail.com>
2018-12-08 22:54:20 -0300

    modci

--- admin/index.php
+++ admin/index.php
@@ -45,9 +45,9 @@ $scripts = ""; ?>
                             </li>
                             </ul>
                             <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
-                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
-                            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+                            <div class="tab-pane fade" id="home" role="tabpanel" aria-labelledby="home-tab">idade</div>
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">nome medico</div>
+                            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">asfjbie</div>
                             </div>
                             </div>
                         </div>
